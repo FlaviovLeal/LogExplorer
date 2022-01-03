48c8884bee615d55267efe8d55b5305dd3094cf5
Flavio Leal <flaviovleal@gmail.com>
2022-01-03 12:58:35 -0300

    add webview hello world

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { HelloWorldPanel } from './HelloWorldPanel';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,15 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('blueprint.helloWorld', () => {
 
-		vscode.window.showInformationMessage('Hello World from Blueprint!');
+		HelloWorldPanel.createOrShow(context.extensionUri);
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('blueprint.HowDay', async () => {
 		let answer = await vscode.window.showInformationMessage('How is your day?', 'good', 'bad');
 		if (answer === 'bad'){
-			vscode.window.showInformationMessage('Sorry')
-		} else if (answer == 'good'){
-			vscode.window.showInformationMessage('Nice')
+			vscode.window.showInformationMessage('Sorry');
+		} else if (answer === 'good'){
+			vscode.window.showInformationMessage('Nice');
 		}
 	}));
 }
